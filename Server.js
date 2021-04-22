3596221b43b7072935d34c92cd88bb56bebd0473
karthiknayak59 <karthiknayak59@gmail.com>
2021-04-21 17:36:25 -0700

    server routing class added

--- Server.js
+++ Server.js
@@ -1,44 +1,56 @@
 import Express from "express";
-//import CourseData from "./CourseData.js";
 import { MongoDbClient } from './DBReader.js';
 
-let dbClient = new MongoDbClient();
-await dbClient.connect();
-await dbClient.listDatabases();
-await dbClient.close();
+class Routes
+{
+    courseData = [
+        {
+            "CourseID" : "A",
+            "CourseName" : "courseA",
+            "Prerequisites" : ["B", "C"]
+        },
+        {
+            "CourseID" : "B",
+            "CourseName" : "courseB",
+            "Prerequisites" : []
+        },
+        {
+            "CourseID" : "C",
+            "CourseName" : "courseC",
+            "Prerequisites" : []
+        }
+    ];
+
+    constructor()
+    {
+
+    }
+
+    Root = (req, res) => {
+        res.json(this.courseData);
+    }
+
+    getcourseid = (req,res)=> {
+        res.json(this.courseData.find((course)=>{
+            return req.params.CourseID === course.CourseID
+        }))  
+    }
+
+}
+
+// Uncomment to try out db connections
+// let dbClient = new MongoDbClient();
+// await dbClient.connect();
+// await dbClient.listDatabases();
+// await dbClient.close();
 
 const app = Express();
 const port = 3000;
-const CourseData = [
-    {
-        "CourseID" : "A",
-        "CourseName" : "courseA",
-        "Prerequisites" : ["B", "C"]
-    },
-    {
-        "CourseID" : "B",
-        "CourseName" : "courseB",
-        "Prerequisites" : []
-    },
-    {
-        "CourseID" : "C",
-        "CourseName" : "courseC",
-        "Prerequisites" : []
-    }
-];
-
-app.get("/", (req,res)=> {
-    res.json(CourseData)
-})
+let routes = new Routes();
+app.get("/", routes.Root)
+app.get("/course/:CourseID", routes.getcourseid);
+app.listen(port, ()=> console.log("listening on port" + port ))
 
 
-app.get("/course/:CourseID", (req,res)=> {
-    res.json(CourseData.find((course)=>{
-        return req.params.CourseID === course.CourseID
-    }))
-    
-})
-
-//app.listen(port, ()=> console.log("listening on port" + port ))
 //app.post()
 //app.delete()
